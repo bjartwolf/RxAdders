25e6c55b8cc32520623e262cc0bcc0d327359e5c
Bjørn Einar Bjartnes <bjartwolf@bjartwolf-desktop.(none)>
2012-10-28 22:52:40 +0100

    getting it sort of right

--- rippleCarry.js
+++ rippleCarry.js
@@ -2,15 +2,16 @@
 var fullAdder = require('./adder.js');
 var Rx = require('rx');
 var orDelay = 10;
-var periodInMs = 1000;
+var periodInMs = 3000;
 // Creating the A and B signals as observable sequences
-var A = Rx.Observable.fromArray([true, false, true, true]);
-var B = Rx.Observable.fromArray([true, true, false, true]);
-var Cin = Rx.Observable.fromArray([false, false, false, false]);
+var timer  = Rx.Observable.interval(periodInMs);
+var A = timer.zip(Rx.Observable.fromArray([false, false, true, true]), function (timer, arrayItem) {return arrayItem;});
+var B = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
+var Cin = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
 
 var fullAdder = new fullAdder(A, B, Cin); 
 
-fullAdder.C.subscribe(function (val) {
+fullAdder.C.delay(10).subscribe(function (val) {
     console.log("C: " + val);
 });
 fullAdder.S.subscribe(function (val) {
