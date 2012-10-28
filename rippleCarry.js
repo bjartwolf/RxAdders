74e32a7253a48478c9365e91e9b0885a61060548
Bjørn Einar Bjartnes <bjartwolf@bjartwolf-desktop.(none)>
2012-10-28 23:10:25 +0100

    think rippleCarry works

--- rippleCarry.js
+++ rippleCarry.js
@@ -5,15 +5,24 @@ var orDelay = 10;
 var periodInMs = 3000;
 // Creating the A and B signals as observable sequences
 var timer  = Rx.Observable.interval(periodInMs);
-var A = timer.zip(Rx.Observable.fromArray([false, false, true, true]), function (timer, arrayItem) {return arrayItem;});
-var B = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
-var Cin = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
+var A1 = timer.zip(Rx.Observable.fromArray([false, false, true, true]), function (timer, arrayItem) {return arrayItem;});
+var A2 = timer.zip(Rx.Observable.fromArray([false, false, true, true]), function (timer, arrayItem) {return arrayItem;});
+var A3 = timer.zip(Rx.Observable.fromArray([false, false, true, true]), function (timer, arrayItem) {return arrayItem;});
+var B1 = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
+var B2 = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
+var B3 = timer.zip(Rx.Observable.fromArray([false, true, false, true]), function (timer, arrayItem) {return arrayItem;});
+var Cin = timer.zip(Rx.Observable.fromArray([false, false, false, false]), function (timer, arrayItem) {return arrayItem;});
 
-var fullAdder = new fullAdder(A, B, Cin); 
+var FA1 = new fullAdder(A1, B1, Cin); 
+var FA2 = new fullAdder(A2, B2, FA1.C); 
+var FA3 = new fullAdder(A3, B3, FA2.C); 
 
-fullAdder.C.delay(10).subscribe(function (val) {
-    console.log("C: " + val);
+FA1.S.subscribe(function (val) {
+    console.log("S1: " + val);
 });
-fullAdder.S.subscribe(function (val) {
-    console.log("S: " + val);
+FA2.S.subscribe(function (val) {
+    console.log("S2: " + val);
+});
+FA3.S.subscribe(function (val) {
+    console.log("S3: " + val);
 });
